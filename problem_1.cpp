5f1c887fa3dd2a5ef6ba1fe45b048765b08fa2d1
Ahmed <ahmedelghzoly618@gmail.com>
2024-11-20 22:14:31 +0200

    c

---
+++ problem_1.cpp
@@ -0,0 +1,19 @@
+//Required: print your name
+
+#include<iostream>
+using namespace std;
+
+
+void PrintName(string name)
+{
+    cout << "Your Name is: " << name << endl;
+}
+
+
+int main()
+{
+
+    PrintName("Ahmed Gamal");
+
+    return 0;
+}
